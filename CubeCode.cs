91f317f09ed5285a67062f9e0524655fa00a99ca
Gunay Bulut <gunaybulut.gc@gmail.com>
2024-02-13 23:47:31 +0300

    first commit

---
+++ CubeCode.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCode : MonoBehaviour
+
+{
+
+    //obje çarptığında olacaklar(sert çarpma-collision)
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("carpti");
+
+        //obje Cube_2 objesine çarpınca Cube_2 nesnesine olacakların yazıldığı kod
+        if (collision.gameObject.name == "Cube_2")
+        {
+            collision.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    //obje çarpma bittiginde olacak olanlar (sert çarpma-collision)
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("carpma bitti");
+    }
+
+    //obje carpma anında olacak olanlar (sert çarpma-collision)
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("carpıyor");
+    }
+
+
+
+
+    //COllision mantıgının aynısı trigger yumusak carma içinde geçerli tek farkı nesneler carparken birbirlerinin içinden geçiyor
+
+    /*//Collider burada çarpılan nesneyi temsil ediyor 
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("yumuşak carpti");
+        //çarpılan nesne log döndürür
+        Debug.Log(collider.gameObject.name+ "cube2 çarpma");
+
+        if (collider.gameObject.name == "Cube_2") {
+            collider.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        Debug.Log("yumuşak carpma bitti");
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        Debug.Log("yumuşak carpıyor");
+    }*/
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+// NOTLAR
+
+//nesnelerde çarpışma olabilmesi için 2 temel kural vardır.
+//Çarpışan tüm nesnelerde "collider" komponenti olmalıdır.
+//çarpışan nesnelerin en az birinde "Rigidbody" komponenti olmalıdır.
+
+//sert çarpıışma (collision)----yolda yürümek
+//yumuşak çarpışma(Trigger)---suya basınca ayağımızın içine girmesi
+
+//OnCollisionEnter ---> ilk çarpışmada bir kez çalışır
+//OnCollisionExit --> çarpışma bittiğinde bir kez çalışır
+//OnCollisionStay --> çarpışma devam ederken sürekli çalışır
+
+/*Unity'de Rigidbody, bir nesnenin fiziksel davranışlarını simüle etmek için kullanılan bir bileşendir. 
+ * Bir oyun nesnesine Rigidbody eklemek, nesnenin fizik yasalarına uygun olarak hareket etmesini sağlar. 
+ * Örneğin, yerçekimi etkisi altında düşme, çarpışmaları işleme alma, ivmelenme ve hızlanma gibi fiziksel etkileri simüle edebilirsiniz.
+ * 
+ * Collider, Unity'de kullanılan bir bileşendir ve nesnelerin çarpışmalarını algılamak için kullanılır. 
+ * Oyun nesnelerinin etkileşimli alanlarını belirlemek için kullanılır. 
+ * Collider bileşenleri, nesnelerin fiziksel varlıklarını temsil eder ve bu nesnelerin birbiriyle etkileşime girmesini sağlar.*/
+
+/*Rigidbody nesnenin fiziksel davranışlarını kontrol ederken Collider nesneler arasındaki 
+ * çarpışmaları algılar ve bu çarpışmaların fiziksel etkileşimlerini yönetir. 
+ * Bu iki bileşen genellikle birlikte kullanılır ve Unity oyun dünyasındaki fiziksel etkileşimlerin modellenmesinde önemli bir rol oynarlar.*/
---
+++ KeybordCubeScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeybordCubeScript : MonoBehaviour
+{
+    GameObject[] nesneler;
+    //bu fonksiyon, oyun nesnesi yaratıldığında ilk çağrılan fonksiyondur. Genellikle bir nesnenin başlangıç durumunu ayarlamak için kullanılır.
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello start");
+        //GetComponent metoduna özel olarak başına gameObject yazmadan kullanılabilir
+        //objenin MeshRenderer componentine erişilir ve işlem yapılır
+        //GetComponent<MeshRenderer>().enabled = false;
+
+        //bir nesneden başka bir nesneye adıyla erişmek için kullanılır 
+        //bir nesneden başka nesnelere erişebilmek için GameObject'i g harfi büyük olarak yazmak gerekir
+        //GameObject.Find("Capsule").GetComponent<MeshRenderer>().enabled = false;
+        //bir nesneden başka bir nesne veya nesnelere tagıyla erişmek için kullanılır
+        //GameObject.FindWithTag("Player").GetComponent<MeshRenderer>().enabled = false;
+        /*
+        nesneler = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject nesne in nesneler)
+        {
+            nesne.GetComponent<MeshRenderer>().enabled = false;
+        }*/
+    }
+
+    //oyun nesnelerinin sürekli olarak(kare basına) güncellenmesi için kullanılır.
+    // Update is called once per frame
+    void Update()
+    {
+
+        //sag ok tusuna basildiginda loglara "sağ ok tuşuna basıldı" yazdırır
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("sağ ok tuşuna basıldı");
+        }
+
+        //sag ok tusundan parmagını çektiginde calıcak olan kod logalra "sağ ok tuşuna basılması bitti" yazdırır
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            Debug.Log("sağ ok tuşuna basılması bitti");
+        }
+
+        //sag ok tusuna basıldıgı sürece calısması istenilen notlar loglara "sağ ok tuşuna basılıyor" yazdırır
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("sağ ok tuşuna basılıyor");
+
+            //update fonksiyonu çalışma hızı bilgisayar gücü ile dogru orantılıdır bu nedenle bu nedenle hareket sırasında farklı pc lerde farklı sonuc almamak için Time.deltaTime kullanılır
+            //1 * Time.deltaTime --> zamana baglı olarak 1 birim ilerle - parantez içinde sırasıyla x-y-z düzlemlerine edecekleri hareket degerleri girilir
+            //-1 * Time.deltaTime şeklinde yazılırsa eksenin tersine gider 
+            //Space.World ---> sahnenin eksenine göre hareket saglar
+            //transform.Translate(1 * Time.deltaTime, 0, 0,Space.World);
+
+            //cismi x ekseninde döndürme işlemi yapılır
+            //transform.Rotate(500 * Time.deltaTime, 0, 0, Space.World);
+
+            //transform metoduna özel olarak başına gameObject yazmadan kullanılabilir
+            //gameObject.transform.Rotate(10, 10, 0);
+
+            //objeyi hareket ettirmek için rigidbody componentine erişip nesneler hareket ettirilebilir
+            //sahnenin x eksenine göre hareket eder(Vector3.right) eğer objenin x eksenine göre hareket etmesi gerekiyorsa transform kullanılır(transform.right)
+            //GetComponent<Rigidbody>().velocity = Vector3.right * 50 * Time.deltaTime;
+
+
+            //gittikçe hızlanan bir şekilde hareket etmesi için AddForce kullanılır
+            GetComponent<Rigidbody>().AddForce(transform.right * 500 * Time.deltaTime);
+
+
+        }
+
+
+        //MAUSE İÇİN
+
+
+        //mousa basildiginda loglara "sol tuşa basıldı" yazdırır
+        //0 mousun sol tusunu 1 ise sag tusunu ifade eder
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("mousun sol tuşuna basıldı");
+        }
+
+        //mousun sol tusundan parmagını çektiginde calıcak olan kod logalra "sağ ok tuşuna basılması bitti" yazdırır
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("mosunun sol tuşuna basılması bitti");
+        }
+
+        //mousun sol tusuna basıldıgı sürece calısması istenilen notlar loglara "sağ ok tuşuna basılıyor" yazdırır
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("mousun sol tuşuna basılıyor");
+        }
+    }
+
+
+}
